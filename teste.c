a5c5d0bd4d17fa7abe84541ba68ca168bdb6f4b6
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2022-02-11 11:14:48 -0300

    teste

--- teste.c
+++ teste.c
@@ -4,11 +4,7 @@
 
 
 int main() {
-    float C,F, K;  
-    printf("Escreva a temperatura em graus celsius: ");//conversao de celsius pra kelvin e fahrenheit
-    scanf("%f",&C);
-    //F = C * 1.8 + 32;//calculo para fahrenheit
-    //K = C + 273.15;//calculo para kelvin
-    printf("\nDe celsius pra Fahrenheit = %.2f \n", F);
-    printf("\nDe celsius pra Kelvin = %.2f \n", K);
+    
+
+
 }
